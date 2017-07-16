2096f6dc4a78029dceb579f9e8ba14ebe36d1e74
yuio1213 <ujungin1213@gmail.com>
2017-07-16 06:05:47 -0700

    dd json parsing prettier

--- get_whois_info.py
+++ get_whois_info.py
@@ -10,30 +10,33 @@ parser = argparse.ArgumentParser(description="args parser")
 parser.add_argument("-f","--file",action="store")
 args = parser.parse_args()
 
-#make exception later 
+#open file and read domains 
 f=open(args.file, "r")
-data=f.read()
-
-#find whois informatino
-domain = whois.query(data)
-parsed = domain.__dict__
-
-#change unparseble data
-if 'last_updated' in parsed: 
-    parsed["last_updated"] = parsed["last_updated"].isoformat()
-if 'expiration_date' in parsed:
-    parsed["expiration_date"] = parsed["expiration_date"].isoformat()
-if 'creation_date' in parsed:
-    parsed["creation_date"] = parsed["creation_date"].isoformat()
-
-if 'name_servers' in parsed:
-    parsed["name_servers"]=list(parsed["name_servers"])
-    print type(parsed["name_servers"])
-
-#print it as jason
-result=json.dumps(parsed)
-print result
-f.close()
 out=open('result.txt','w')
-out.write(result)
+
+line=f.readline()
+print "line:",line
+
+while line:
+    #find whois informatino
+    domain = whois.query(line)
+    parsed = domain.__dict__
+    
+    #change unparseble data
+    if 'last_updated' in parsed: 
+        parsed["last_updated"] = parsed["last_updated"].isoformat()
+    if 'expiration_date' in parsed:
+        parsed["expiration_date"] = parsed["expiration_date"].isoformat()
+    if 'creation_date' in parsed:
+        parsed["creation_date"] = parsed["creation_date"].isoformat()
+
+    if 'name_servers' in parsed:
+        parsed["name_servers"]=list(parsed["name_servers"])
+    
+    result=json.dumps(parsed, indent=4,sort_keys=True)
+    out.write(result)
+    out.write("\n")
+    line=f.readline()
+
+f.close()
 out.close()
